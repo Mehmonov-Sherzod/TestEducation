4992d9377dff0dd4c3827b13db11bc5996aaa506
Mehmonov-Sherzod <mehmovovsherzod@gmail.com>
2025-09-25 19:43:45 +0500

    enum qowildi

---
+++ TestEducation/Models/Enum/PermissionEnum.cs
@@ -0,0 +1,40 @@
+﻿namespace TestEducation.Models.Enum
+{
+    public enum  PermissionEnum
+    {
+        // Subject
+        ViewSubject,
+    CreateSubject,
+    UpdateSubject,
+    DeleteSubject,
+
+    // Test
+    ViewTest,
+    CreateTest,
+    UpdateTest,
+    DeleteTest,
+
+    // Question
+    ViewQuestion,
+    CreateQuestion,
+    UpdateQuestion,
+    DeleteQuestion,
+
+    // Answer Options
+    ViewAnswerOption,
+    CreateAnswerOption,
+    UpdateAnswerOption,
+    DeleteAnswerOption,
+
+    // Student
+    ViewStudent,
+    CreateStudent,
+    UpdateStudent,
+    DeleteStudent,
+
+    // Student Test
+    AssignTestToStudent,
+    TakeTest,
+    ViewResults
+    }
+}
