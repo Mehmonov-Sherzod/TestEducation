22563088ab8bb2839e41d0298e8cc11cab99a67d
Mehmonov-Sherzod <mehmovovsherzod@gmail.com>
2025-09-26 18:13:39 +0500

    registir

---
+++ TestEducation/Controllers/AuthService/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestEducation.Data;
+using TestEducation.Dtos;
+using TestEducation.Service;
+
+namespace TestEducation.Controllers.AuthService
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase 
+    {
+        private readonly JwtService jwtService;
+
+        private readonly AppDbContext appDbContext;
+
+        public AuthController(JwtService _jwtService, AppDbContext _appDbContext )
+        {
+            jwtService = _jwtService;
+            appDbContext = _appDbContext;
+        }
+
+
+        //[HttpPost("Regist")]
+
+        //public IActionResult UserCreate(UserDto userDto)
+        //{
+        //    if (userDto == null)
+        //    {
+
+        //    }
+
+        //}
+
+
+
+    }
+}
