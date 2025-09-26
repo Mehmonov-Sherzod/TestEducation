f02f13de719e4e3a30fe47479806cb722b2ad043
Egamberdiyev03 <egamberdiyevasadbek207@gmail.com>
2025-09-26 17:19:33 +0500

    Demo Class yaratildi

---
+++ TestEducation/DemoClass.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TestEducation
+{
+    public class DemoClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
