4f6b23bdabb465bf2798c8a211e366d3a2410dec
Mehmonov-Sherzod <mehmovovsherzod@gmail.com>
2025-09-29 00:34:03 +0500

    enum dan bazaga yuklash

--- TestEducation/Program.cs
+++ TestEducation/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using TestEducation.Data;
+using TestEducation.Models;
+using TestEducation.Models.Enum;
 using TestEducation.Service;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +15,8 @@ builder.Services.AddControllers();
 
 builder.Services.AddScoped<JwtService>();
 builder.Services.AddScoped<AppDbContext>();
+
+
 
 // JWT Authentication
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -31,12 +36,32 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddAuthorization();
 
 
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+//using (var scope = app.Services.CreateScope())
+//{
+//    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+//    var permissions = Enum.GetValues(typeof(PermissionEnum))
+//        .Cast<PermissionEnum>()
+//        .Select(p => new Permission
+//        {
+//            Name = p.ToString(),
+//            Description = p + " permission"
+//        }).ToList();
+
+//    foreach (var perm in permissions)
+//    {
+//        if (!context.permissions.Any(x => x.Name == perm.Name))
+//            context.permissions.Add(perm);
+//    }
+
+//    context.SaveChanges();
+//}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
