56b0b860ba9705c2b6d09f7097e35cce0333b35c
Mehmonov-Sherzod <mehmovovsherzod@gmail.com>
2025-09-28 17:41:08 +0500

    Subject Controller

--- TestEducation/Controllers/SubjectController.cs
+++ TestEducation/Controllers/SubjectController.cs
@@ -1,6 +1,44 @@
-﻿namespace TestEducation.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using TestEducation.Data;
+using TestEducation.Dtos;
+using TestEducation.Models;
+using TestEducation.Service;
+
+namespace TestEducation.Controllers
 {
-    public class SubjectController
+    [ApiController]
+    [Route("[controller]")]
+    public class SubjectController : ControllerBase 
     {
+        private readonly AppDbContext appDbContext;
+
+        public SubjectController( AppDbContext _appDbContext)
+        {
+          
+            appDbContext = _appDbContext;
+        }
+
+        [HttpPost]
+
+        public IActionResult SubjectCreate(SubjectDto subjectDto)
+        {
+
+            if (subjectDto  == null)
+            {
+                NotFound("hato");
+            }
+
+            var Subject = new Subject
+            {
+                Name = subjectDto.Name
+            };
+
+           appDbContext.subjects.Add(Subject);
+           appDbContext.SaveChanges();
+
+            return Ok("Subject Qoshildi");
+
+        }
+
     }
 }
